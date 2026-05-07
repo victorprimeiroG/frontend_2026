afc44ace2f634ed559cc6b8c9667206cc4e31d46
victorprimeiroG <110112719+victorprimeiroG@users.noreply.github.com>
2026-05-07 09:35:01 -0300

    Update script.js

--- vaiprogit/sript.js
+++ vaiprogit/sript.js
@@ -2,14 +2,23 @@ const textInput = document.getElementById('taskInput');
 const textList = document.getElementById('taskList');
 
 function addTask(){
-  const temTexto = textInput.value.trim();
-  if(temTexto !== ''){
-    const taskAdd = document.createElement('li');
-    taskAdd.innerHTML = 
-    `<span>${temTexto}</span>
-    <button onclick="">editar<button>
-    <button onclick="">remover<button>
-    <button onclick="">confirmar<button>`
-    taskList.appendChild(taskAdd);
+  const textInput = taskInput.value.trim();
+  if(textInput !== ''){
+    const newItem = document.createElement('li');
+    newItem.innerHTML = 
+    `<span>${textInput}</span>
+    <button onclick="editar()">editar</button>
+    <button onclick="remover(this)">remover</button>
+    <button onclick="concluir(this)">confirmar</button>`
+    taskList.appendChild(newItem);
   }
+}
+
+function remover(button){
+  const itemToRemove = button.parentElement;
+  taskList.removeChild(itemToRemove);
+}
+function concluir(button){
+  const itemToMark = button.parentElement;
+  itemToMark.classList = 'completed';
 }
